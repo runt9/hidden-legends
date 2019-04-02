7119000c6be61eaa08571992e7cbaa8192ab0772
avester <runt9ab@gmail.com>
2019-04-02 14:40:30 -0500

    Try a completely different format for Travis

--- build.sh
+++ build.sh
@@ -3,8 +3,5 @@
 set -eux
 export PATH=.:$PATH
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
-sudo apt-get update && sudo apt-get install -y nodejs openjdk-8-jdk
-sudo npm i -g yarn
 (cd src/jsMain && yarn install)
 ./gradlew jvmTest jsTest bootJar
