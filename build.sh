efd3245ba426ed7bd4d6d43e3088454db4895017
avester <runt9ab@gmail.com>
2019-04-02 14:12:22 -0500

    Use build script for Travis build

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 set -eux
 export PATH=.:$PATH
 
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get update && apt-get install -y nodejs
+npm i -g yarn
 (cd src/jsMain && yarn install)
 ./gradlew jvmTest jsTest bootJar
